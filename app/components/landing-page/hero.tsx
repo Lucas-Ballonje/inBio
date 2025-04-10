291fdd8c960403ddd431f33ad4fdaa4394f613e4
Lucas-Ballonje <lucasballonje12@gmail.com>
2025-04-10 17:15:59 -0300

    fix header e hero

--- app/components/landing-page/hero.tsx
+++ app/components/landing-page/hero.tsx
@@ -6,7 +6,7 @@ import ProjectCard from "@/app/commons/project-card";
 
 export default function Hero(){
     return(
-        <div className="flex border h-screen">
+        <div className="flex h-screen">
             <div className="w-full flex flex-col gap-2 mt-[35vh]">
                 <h1 className="text-5xl font-bold text-white leading-[64px]">Seus projetos e redes sociais em um único link</h1>
                 <h2 className="text-xl leading-6">
