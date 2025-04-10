82fd6141de55323282cb7fae96b71fa96a9eb063
Lucas-Ballonje <lucasballonje12@gmail.com>
2025-04-10 15:36:13 -0300

    fix finalizando hero

--- app/commons/use-card.tsx
+++ app/commons/use-card.tsx
@@ -1,11 +1,20 @@
 import { Facebook, Github, Linkedin, Twitter, Plus, Instagram } from "lucide-react";
-import Button from "../components/ui/butto";
+import Button from "../components/ui/button";
 
 export default function UserCard(){
+
+    const icons = [        
+        Github,
+        Linkedin,
+        Twitter,
+        Instagram,
+        Plus
+    ]
+    
     return(
         <div className="w[348px] flex flex-col gap-5 items-center p-5 border border-white border-opacity-10 bg-[#121212] rounded-3xl text-white">
             <div className="size-48">
-                <img src="/" className="rounded-full object-cover w-full h-full" alt="User Avatar" />
+                <img src="/me.webp" className="rounded-full object-cover w-full h-full" alt="User Avatar" />
             </div>
             <div className="flex flex-col gap-2 w-full">
                 <div className="flex items-center gap-2">
@@ -18,23 +27,23 @@ export default function UserCard(){
             <div className="flex flex-col gap-2 w-full">
                 <span className="uppercase text-xs font-medium">Links</span>
                 <div className="flex gap-3">
-                    <Github />
-                    <Instagram />
-                    <Facebook />
-                    <Linkedin />
-                    <Twitter />
-                    <Plus />
+                    {icons.map((Icon, index) => {
+                        return (
+                            <button key={index} className="p-3 rounded-xl bg-[#1e1e1e] hover:bg-[#2e2e2e]">
+                                <Icon />
+                            </button>
+                        )
+                    })}                  
                 </div>
             </div>
             <div className="flex flex-col gap-3 w-full h-[172px]">
                 <div className="w-full flex flex-col items-center gap-3">
-                    <Button>Template SaaS - Compre Agora</Button>
-                    <button>
+                    <Button className="w-full">Template SaaS - Compre Agora</Button>
+                    <button className="p-3 rounded-xl bg-[#1e1e1e] hover:bg-[#2e2e2e]">
                         <Plus/>
                     </button>
 
                 </div>
-
             </div>
         </div>
     )
--- app/components/landing-page/hero.tsx
+++ app/components/landing-page/hero.tsx
@@ -1,9 +1,12 @@
-import Button from "../ui/butto";
+import UserCard from "@/app/commons/use-card";
+import Button from "../ui/button";
 import TextInput from "../ui/text-input";
+import { TotalVisits } from "@/app/commons/total-visits";
+import ProjectCard from "@/app/commons/project-card";
 
 export default function Hero(){
     return(
-        <div className="flex border">
+        <div className="flex border h-screen">
             <div className="w-full flex flex-col gap-2 mt-[35vh]">
                 <h1 className="text-5xl font-bold text-white leading-[64px]">Seus projetos e redes sociais em um único link</h1>
                 <h2 className="text-xl leading-6">
@@ -16,17 +19,17 @@ export default function Hero(){
                     <Button>Criar Agora</Button>
                 </div>
             </div>
-            <div className="w-full flex items-center justigy-center bg-[radial-gradient(circle_at_50%_50%,#482DBB, transparent_55%)]">
+            <div className="w-full flex items-center justify-center bg-[radial-gradient(circle_at_50%_50%,#482DBB,transparent_55%)]">
                 <div className="relative">
-                    {/* <UserCard/> */}
+                    <UserCard/>
                     <div className="absolute -bottom-[7%] -right-[45%]">
-                        {/* <TotalVisits/> */}
+                        <TotalVisits/>
                     </div>
                     <div className="absolute top-[20%] -left-[45%] -z-10">
-                        {/* <ProjectCard/> */}
+                        <ProjectCard/>
                     </div>
                     <div className="absolute -top-[5%] -left-[55%] -z-10">
-                        {/* <ProjectCard/> */}
+                        <ProjectCard/>
                     </div>
 
                 </div>
