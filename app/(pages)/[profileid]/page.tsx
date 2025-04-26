07d58432a82871b62fe9710125ccc40c06cbc18e
Lucas-Ballonje <lucasballonje12@gmail.com>
2025-04-26 17:33:15 -0300

    feat pagina completa sem funcionalidade

---
+++ app/(pages)/[profileid]/page.tsx
@@ -0,0 +1,41 @@
+import ProjectCard from "@/app/commons/project-card";
+import { TotalVisits } from "@/app/commons/total-visits";
+import UserCard from "@/app/commons/use-card";
+import { Plus } from "lucide-react";
+import Link from "next/link";
+
+export default async function ProfilePage({
+  params,
+}: {
+  params: { profileid: string };
+}) {
+  const { profileid } = await params;
+
+  return (
+    <div className="relative h-screen flex p-20 overflow-hidden">
+      <div className="fixed top-0 left-0 w-full flex justify-center items-center gap-1 py-2 bg-background-tertiary">
+        <span>Você esta usando a versão trial</span>
+        <Link href={`/${profileid}/upgrade`}>
+          <button className="text-accent-green font-bold">
+            Faça o upgrade agora
+          </button>
+        </Link>
+      </div>
+      <div className="w-1/2 flex justify-center h-min">
+        <UserCard />
+      </div>
+      <div className="w-full flex justify-center content-start gap-4 flex-wrap overflow-y-auto">
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <button className="w-[340px] h-[132px] rounded-[20px] bg-background-secondary flex items-center gap-2 justify-center hover:border border-dashed border-background-secondary">
+          <Plus className="size-10" />
+          <span className="text-accent-green">Criar novo projeto</span>
+        </button>
+      </div>
+      <div className="absolute bottom-4 right-0 left-0 w-min mx-auto">
+        <TotalVisits />
+      </div>
+    </div>
+  );
+}
